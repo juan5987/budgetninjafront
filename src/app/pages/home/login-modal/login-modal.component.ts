cbc17eacfff9a8519e2f5b22d2b22152c1bb1fe8
Juan <juan.machado.5987@gmail.com>
2023-04-27 16:58:30 +0200

    added form Validator

--- src/app/pages/home/login-modal/login-modal.component.ts
+++ src/app/pages/home/login-modal/login-modal.component.ts
@@ -1,4 +1,4 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
 import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
@@ -6,9 +6,15 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
   templateUrl: './login-modal.component.html',
   styleUrls: ['./login-modal.component.sass']
 })
-export class LoginModalComponent {
+
+export class LoginModalComponent  implements OnInit{
   submitted: boolean = false;
   isModalOpen = false;
+  formValues: FormGroup = this.formBuilder.group({
+    email: ['', [Validators.required, Validators.email]],
+    password: ['', Validators.required],
+  })
+  
   @Output() onCloseLoginModal:EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private formBuilder: FormBuilder) { };
@@ -19,16 +25,21 @@ export class LoginModalComponent {
 
   handleSubmit(e: Event) {
     e.preventDefault();
-    console.log('submit');
+    this.submitted = true;
+    //TODO: REQUETTE HTTP
   }
 
   stopPropagation(e: Event) {
     e.stopPropagation();
-  }
+  }  
 
-  // Formulaire de connexion
-  formValues: FormGroup = this.formBuilder.group({
-    email: ['', [Validators.required, Validators.email]], // je peux mettre un ou plusieurs validateur(s)
-    password: ['', Validators.required], // je peux mettre un ou plusieurs validateur(s)
+  ngOnInit() {
+  this.formValues.valueChanges.subscribe(() => {
+    this.submitted = false;
   })
+}
+
+  public get form() {
+    return this.formValues.controls;
+  }
 }
--- src/app/pages/home/signup-modal/signup-modal.component.ts
+++ src/app/pages/home/signup-modal/signup-modal.component.ts
@@ -1,14 +1,26 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit } from '@angular/core';
+import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-modal',
   templateUrl: './signup-modal.component.html',
   styleUrls: ['./signup-modal.component.sass']
 })
-export class SignupModalComponent {
+export class SignupModalComponent implements OnInit {
   isModalOpen = false;
+  submitted: boolean = false;
+  
+  formValues: FormGroup = this.formBuilder.group({
+    username: ['', [Validators.required, Validators.minLength(3)]],
+    email: ['', [Validators.required, Validators.email]],
+    emailConfirm: ['', [Validators.required, Validators.email]],
+    password: ['', [Validators.required, Validators.minLength(8)]],
+    passwordConfirm: ['', [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[0-9])(?=.*[!@#$%^&*()])(?=.*[A-Z])(?=.*[a-z]).+$")]],
+  })
   
   @Output() onCloseSignupModal:EventEmitter<boolean> = new EventEmitter<boolean>();
+
+  constructor(private formBuilder: FormBuilder) { };
 
   closeModal() {
     this.onCloseSignupModal.emit();
@@ -16,10 +28,21 @@ export class SignupModalComponent {
 
   handleSubmit(e: Event) {
     e.preventDefault();
+    this.submitted = true;
     console.log('submit');
   }
 
   stopPropagation(e: Event) {
     e.stopPropagation();
+  }
+
+  ngOnInit() {
+    this.formValues.valueChanges.subscribe(() => {
+      this.submitted = false;
+    })
+  }
+
+  public get form() {
+    return this.formValues.controls;
   }
 }
