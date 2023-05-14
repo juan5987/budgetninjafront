f73469380c09a2c09d62b6d9054295a9ef03ba7e
Juan5987 <juan.machado.5987@gmail.com>
2023-05-14 22:05:50 +0200

    added custom validator for email confirmation

--- src/app/pages/home/components/signup-modal/signup-modal.component.ts
+++ src/app/pages/home/components/signup-modal/signup-modal.component.ts
@@ -1,5 +1,5 @@
 import { Component, Output, EventEmitter, OnInit } from '@angular/core';
-import { FormGroup, Validators, FormBuilder } from '@angular/forms';
+import { FormGroup, Validators, FormBuilder, ValidatorFn, AbstractControl, ValidationErrors, FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-signup-modal',
@@ -9,15 +9,24 @@ import { FormGroup, Validators, FormBuilder } from '@angular/forms';
 export class SignupModalComponent implements OnInit {
   isModalOpen = false;
   submitted: boolean = false;
-  
+
+  isConfirmed = (input: FormControl) => {
+    const email = this.formValues?.get('email')?.value;
+    const emailConfirm = this.formValues?.get('emailConfirm')?.value;
+    const isConfirmed = email === emailConfirm;
+    return isConfirmed ? null : { notConfirmed: true };
+  }
+
   formValues: FormGroup = this.formBuilder.group({
     username: ['', [Validators.required, Validators.minLength(3)]],
-    email: ['', [Validators.required, Validators.email]],
-    emailConfirm: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email, this.isConfirmed]],
+    emailConfirm: ['', [Validators.required, Validators.email, this.isConfirmed]],
     password: ['', [Validators.required, Validators.minLength(8)]],
     passwordConfirm: ['', [Validators.required, Validators.minLength(8), Validators.pattern("^(?=.*[0-9])(?=.*[!@#$%^&*()])(?=.*[A-Z])(?=.*[a-z]).+$")]],
   })
-  
+
+
+
   @Output() onCloseSignupModal:EventEmitter<boolean> = new EventEmitter<boolean>();
 
   constructor(private formBuilder: FormBuilder) { };
@@ -29,7 +38,6 @@ export class SignupModalComponent implements OnInit {
   handleSubmit(e: Event) {
     e.preventDefault();
     this.submitted = true;
-    console.log('submit');
   }
 
   stopPropagation(e: Event) {
