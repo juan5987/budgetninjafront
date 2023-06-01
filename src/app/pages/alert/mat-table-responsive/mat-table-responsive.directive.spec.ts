105196d5849f03b33fd716e966373f116a64ce62
Olivia-P <piganiolivia@gmail.com>
2023-06-01 16:30:10 +0200

    before pulling dev

--- src/app/pages/alert/mat-table-responsive/mat-table-responsive.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { MatTableResponsiveDirective } from './mat-table-responsive.directive';
-
-describe('MatTableResponsiveDirective', () => {
-  it('should create an instance', () => {
-    const directive = new MatTableResponsiveDirective();
-    expect(directive).toBeTruthy();
-  });
-});
