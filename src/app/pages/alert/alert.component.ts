8088926fd7a81f644ce6379623fe01ce210d965e
Olivia-P <piganiolivia@gmail.com>
2023-06-06 13:36:44 +0200

    must kill angular material table and replace it by html/css table

--- src/app/pages/alert/alert.component.ts
+++ src/app/pages/alert/alert.component.ts
@@ -63,7 +63,7 @@ export class AlertComponent implements OnInit{
 editAlert(row : any){
   this.dialog.open(AddAlertComponent, {
     width:'50%',
-    height:'84.2%',
+    height:'86%',
     data : row }).afterClosed().subscribe(val=>{
       if(val === 'mettre à jour'){
         this.getAllAlert();
