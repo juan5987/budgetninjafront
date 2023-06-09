6b4f2c5ff8f51e48647ac1eb94e422cdcc1e0e57
Olivia-P <piganiolivia@gmail.com>
2023-06-09 16:04:45 +0200

    formControl and formGroup ok, now dropdown menu css  fighting

--- src/app/pages/saving/add-project-modal/add-project-modal.component.ts
+++ src/app/pages/saving/add-project-modal/add-project-modal.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
-
+import {Component, Inject, OnInit} from '@angular/core';
+import {FormGroup, FormBuilder, Validator, Validators} from "@angular/forms";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
 
 @Component({
@@ -7,7 +8,24 @@ import { Component } from '@angular/core';
   templateUrl: './add-project-modal.component.html',
   styleUrls: ['./add-project-modal.component.css']
 })
-export class AddProjectModalComponent {
+export class AddProjectModalComponent implements OnInit{
+
+  savingForm ! : FormGroup;
+
+
+  constructor(private formBuilder : FormBuilder,
+              @Inject(MAT_DIALOG_DATA) public editData : any,){}
+
+
+  ngOnInit() {
+    this.savingForm=this.formBuilder.group({
+      objEpargne:['', Validators.required],
+      amountToGet:['', Validators.required],
+      yesOrNo:['',Validators.required],
+      endDate:['',Validators.required]
+
+    })
+  }
 
 
 }
