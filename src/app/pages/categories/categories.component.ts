b7bd17ae9dbd2e44bf99cb1bfc36c2f3f6b0c3b9
duong <duongngo0103@gmail.com>
2023-06-19 09:14:13 +0200

    commit before sưitch

--- src/app/pages/categories/categories.component.ts
+++ src/app/pages/categories/categories.component.ts
@@ -1,80 +1,84 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
-import {
-  MatDialog,
-} from '@angular/material/dialog';
+import { MatDialog } from '@angular/material/dialog';
 import { DialogComponent } from './dialog/dialog.component';
 import { ApiService } from 'src/app/services/api.service';
-import { MatPaginator} from '@angular/material/paginator';
-import { MatSort} from '@angular/material/sort';
-import { MatTableDataSource} from '@angular/material/table';
+import { MatPaginator } from '@angular/material/paginator';
+import { MatSort } from '@angular/material/sort';
+import { MatTableDataSource } from '@angular/material/table';
 
 @Component({
   selector: 'app-categories',
   templateUrl: './categories.component.html',
   styleUrls: ['./categories.component.css']
 })
-export class CategoriesComponent {
-   
+export class CategoriesComponent implements OnInit {
   displayedColumns: string[] = ['categorieName', 'ajouter'];
   dataSource!: MatTableDataSource<any>;
 
   @ViewChild(MatPaginator) paginator!: MatPaginator;
   @ViewChild(MatSort) sort!: MatSort;
+
+  userId: number = 123; // Remplacez 123 par l'userId souhaité
+
   constructor(private dialog: MatDialog, private api: ApiService) {}
+
   ngOnInit(): void {
     this.getAllCategories();
   }
+
   openDialog() {
     this.dialog.open(DialogComponent, {
       width: '50%',
-    }).afterClosed().subscribe(val=>{
-      if(val==='Enregistrer'){
+    }).afterClosed().subscribe(val => {
+      if (val === 'Enregistrer') {
         this.getAllCategories();
       }
     });
   }
+
   getAllCategories() {
-    this.api.getCategorie().subscribe({
+    this.api.getAllCategoriesByUserId(this.userId).subscribe({
       next: (res) => {
         this.dataSource = new MatTableDataSource(res);
         this.dataSource.paginator = this.paginator;
         this.dataSource.sort = this.sort;
       },
       error: (err) => {
-        alert('error while fetching the Records ');
+        alert('Error while fetching the records');
       },
     });
   }
-  editCategorie(row: any){
-    this.dialog.open(DialogComponent,{
+
+  editCategorie(row: any) {
+    this.dialog.open(DialogComponent, {
       width: '30%',
-      data:row
-    }).afterClosed().subscribe(val=>{
-      if(val==='update'){
+      data: row
+    }).afterClosed().subscribe(val => {
+      if (val === 'update') {
         this.getAllCategories();
       }
-    })
+    });
   }
-  deleteCategorie(id:number){
-    this.api.deteleCategorie(id)
-    .subscribe({
-      next:(res)=>{
-        alert("Product deleted ");
-        this.getAllCategories();
-      },
-      error: ()=>{
-        alert("Error while deleting")
-      }
-    })
 
+  deleteCategorie(id: number) {
+    this.api.deleteCategorie(id)
+      .subscribe({
+        next: (res) => {
+          alert('Product deleted');
+          this.getAllCategories();
+        },
+        error: () => {
+          alert('Error while deleting');
+        }
+      });
   }
-  applyFilter(event: Event) {
-    const filterValue = (event.target as HTMLInputElement).value;
-    this.dataSource.filter = filterValue.trim().toLowerCase();
 
-    if (this.dataSource.paginator) {
-      this.dataSource.paginator.firstPage();
-    }
-  }
-  
+  // applyFilter(event: Event) {
+  //   const filterValue = (event.target as HTMLInputElement).value;
+  //   this.dataSource.filter = filterValue.trim().toLowerCase();
+
+  //   if (this.dataSource.paginator) {
+  //     this.dataSource.paginator.firstPage();
+  //   }
+  // }
 }
--- src/app/services/api.service.ts
+++ src/app/services/api.service.ts
@@ -1,20 +1,29 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   constructor( private http: HttpClient) { }
-  postCategorie(data : any){
-    return this.http.post<any>("http://localhost:8080/categorieList",data)
+  postCategorie(data: any): Observable<any> {
+    return this.http.post<any>("http://localhost:8080/categories", data);
   }
-  getCategorie(){
-    return this.http.get<any>("http://localhost:8080/categorieList");
-}
-putCategorie(data:any, id:number ){
-return this.http.post<any>("http://localhost:8080/categorieList/"+id,data)
-}
-deteleCategorie(id:number){
-  return this.http.delete<any>("http://localhost:8080/categorieList/"+id)
-}
+
+  getCategorie(): Observable<any> {
+    return this.http.get<any>("http://localhost:8080/categories");
+  }
+
+  putCategorie(data: any, id: number): Observable<any> {
+    return this.http.put<any>("http://localhost:8080/categories/" + id, data);
+  }
+
+  deleteCategorie(id: number): Observable<any> {
+    return this.http.delete<any>("http://localhost:8080/categories/" + id);
+  }
+
+  getAllCategoriesByUserId(userId: number): Observable<any[]> {
+    const url = `http://localhost:8080/categories/${userId}`;
+    return this.http.get<any[]>(url);
+  }
 }
